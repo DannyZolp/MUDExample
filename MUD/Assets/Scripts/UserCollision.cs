f14b2ef7750ea838003e88696cdc5f4df3be36c1
Danny Zolp <dannydog5186@gmail.com>
2020-04-26 15:48:59 -0500

    Infinite Room Generation
    Allowing you to go in one direction forever and it will generate a new room.

---
+++ MUD/Assets/Scripts/UserCollision.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserCollision : MonoBehaviour
+{
+    public static GameObject RoomIn;
+    private GameObject FPV;
+    private string DoorMaterial;
+    private string RoomMaterial;
+
+    void Start()
+    {
+        this.FPV = GameObject.Find("FPV");
+        this.DoorMaterial = GameObject.Find("DoorXPOS").GetComponent<Renderer>().material.name;
+        this.RoomMaterial = GameObject.Find("WallXPOS").GetComponent<Renderer>().material.name;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject colliderGO = collision.collider.gameObject;
+        if (colliderGO.transform.parent.gameObject.tag == GameObject.Find("Home").gameObject.tag)
+        {
+            RoomIn = colliderGO.transform.parent.gameObject;
+            //Debug.Log("Found New Room " + RoomIn.name);
+        }
+    }
+}
